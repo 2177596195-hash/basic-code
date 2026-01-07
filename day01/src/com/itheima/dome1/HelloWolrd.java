d6a2f6dac0a946f83f7324f1bd7f669ee77d67d3
X_xiaomi <2177596195@qq.com>
2026-01-07 15:47:00 +0800

    111

---
+++ day01/src/com/itheima/dome1/HelloWolrd.java
@@ -0,0 +1,13 @@
+package com.itheima.dome1;
+
+public class HelloWolrd {
+        //mian方法
+        public static  void main(String[] args){
+            System.out.println("HolleWorld");
+            //输出HolleWorld " "字符串，' '字符类型，内容只有一个
+
+        }
+        /*class：定义一个类 后 类名
+         \t 制表符:打印的时候将字符串长度补齐到8，或8的整数，至少补一个空格，最多部8个
+        */
+}
